d8beac088a49bd8db08564cd5905acfea56275d2
Alexey Kovalev <41exey.kov41ev@gmail.com>
2020-05-20 13:18:23 +0300

    7 tasks

---
+++ course 1/practice on programming/final_work/zad_7.py
@@ -0,0 +1,41 @@
+from math import pi, atan
+from decimal import Decimal
+from check import get_x, get_eps, get_n
+
+
+def arctg_(x, n=10, eps=None):  # рекуррентная магия
+    x = Decimal(x)
+    a = Decimal('1.0')
+    b = x
+    c = Decimal('-1.0')
+    res = c / (a * b)
+    if eps:
+        tmp = 0
+        i = 1
+        while abs(res - tmp) > eps:
+            tmp = res
+            a += 2
+            b = b * x ** 2
+            c = -c
+            res += c / (a * b)
+            i += 1
+        try:
+            r = len(str(eps).split('.')[1])
+        except IndexError:
+            r = int(str(eps).split('-')[1])
+        return round(Decimal(pi / 2) + res, r)
+    for i in range(1, n):
+        a += 2
+        b = b * x ** 2
+        c = -c
+        res += c / (a * b)
+    return Decimal(pi / 2) + res
+
+
+if __name__ == '__main__':
+    x = get_x()
+    eps = get_eps()
+    result = arctg_(x, eps=eps)  # мой результат
+    true_res = atan(x)  # настоящий результат
+    print(f'artg({x}) = {result}, с точностью {eps}')
+    print(f'artg({x}) = {true_res}, вычисления, полученные с помощью math')
