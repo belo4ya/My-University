d559def0b641868915b2cbde23b257ea6ccccca3
Alexey Kovalev <41exey.kov41ev@gmail.com>
2020-05-20 18:15:02 +0300

    Done

--- course 1/practice on programming/final_work/zad_6.py
+++ course 1/practice on programming/final_work/zad_6.py
@@ -1,13 +1,11 @@
 from math import pi
-from decimal import Decimal
 from check import get_x, get_eps, get_n
 
 
 def arcctg_(x, n=10, eps=None):  # рекуррентная магия
-    x = Decimal(x)
     a = x
-    b = Decimal('1.0')
-    c = Decimal('-1.0')
+    b = 1.0
+    c = -1.0
     res = c * a / b
     if eps:
         tmp = 0
@@ -23,17 +21,21 @@ def arcctg_(x, n=10, eps=None):  # рекуррентная магия
             r = len(str(eps).split('.')[1])
         except IndexError:
             r = int(str(eps).split('-')[1])
-        return round(Decimal(pi / 2) + res, r)
+        return round(pi / 2 + res, r)
     for i in range(1, n):
         a = a * x ** 2
         b += 2
         c = -c
         res += c * a / b
-    return Decimal(pi / 2) + res
+    return pi / 2 + res
 
 
 if __name__ == '__main__':
-    x = get_x()
+    while True:
+        print('Введите |x| <= 1')
+        x = get_x()
+        if abs(x) <= 1:
+            break
     eps = get_eps()
     result = arcctg_(x, eps=eps)  # мой результат
     print(f'arcctg({x}) = {result}, с точностью {eps}')
