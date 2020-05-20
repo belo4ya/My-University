e4823e2075567e50e66902dd7c060f30480e01a7
Alexey Kovalev <41exey.kov41ev@gmail.com>
2020-05-20 14:02:55 +0300

    6, 13 don't work

---
+++ course 1/practice on programming/final_work/zad_8.py
@@ -0,0 +1,41 @@
+from math import cos
+from decimal import Decimal
+from check import get_x, get_eps, get_n
+
+
+def cos_(x, n=10, eps=None):  # рекуррентная магия
+    x = Decimal(x)
+    a = Decimal('1.0')
+    b = Decimal('1.0')
+    c = Decimal('1.0')
+    res = c * a / b
+    if eps:
+        tmp = 0
+        i = 1
+        while abs(res - tmp) > eps:
+            tmp = res
+            a = a * x ** 2
+            b = b * (2 * i * (2 * i - 1))
+            c = -c
+            res += c * a / b
+            i += 1
+        try:
+            r = len(str(eps).split('.')[1])
+        except IndexError:
+            r = int(str(eps).split('-')[1])
+        return round(res, r)
+    for i in range(1, n):
+        a = a * x ** 2
+        b = b * (2 * i * (2 * i - 1))
+        c = -c
+        res += c * a / b
+    return res
+
+
+if __name__ == '__main__':
+    x = get_x()
+    n = get_n()
+    result = cos_(x, n=n)  # мой результат
+    true_res = cos(x)  # настоящий результат
+    print(f'cos({x}) = {result}, для n = {n}')
+    print(f'cos({x}) = {true_res}, вычисления, полученные с помощью math')
