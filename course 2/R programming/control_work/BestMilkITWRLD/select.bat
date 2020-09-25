f73cca950f17dc31e10edd7e679fbc6f32231d36
Alexey Kovalev <41exey.kov41ev@gmail.com>
2020-09-25 22:18:35 +0300

    part1

---
+++ course 2/R programming/control_work/BestMilkITWRLD/select.bat
@@ -0,0 +1,17 @@
+chcp 65001
+rem @echo off
+setlocal EnableDelayedExpansion
+
+set hh=%TIME:~0,2%
+set mm=%TIME:~3,2%
+set ss=%TIME:~6,2%
+echo %hh%:%mm%:%ss% %DATE% > ".\Анализ\log.txt"
+echo ------------------------------------------- >> ".\Анализ\log.txt"
+
+for /L %%i in (1,1,10) do (
+    set "formattedValue=000000%%i"
+
+	echo %%i >> ".\Анализ\log.txt"
+	(copy /y/v ".\Магазин%%i\in.txt" ".\Анализ\store!formattedValue:~-3!_in.txt") >> ".\Анализ\log.txt"
+	(copy /y/v ".\Магазин%%i\out.txt" ".\Анализ\store!formattedValue:~-3!_out.txt") >> ".\Анализ\log.txt"
+)
