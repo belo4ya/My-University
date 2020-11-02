817ec353c67c07557fd2d5edd45b675b9bfb7462
Alexey Kovalev <41exey.kov41ev@gmail.com>
2020-11-03 00:34:13 +0300

    inheritancePrac2

---
+++ course II/JAVA programming/inheritancePrac2/src/com/java/transportCards/TravelPass.java
@@ -0,0 +1,12 @@
+package com.java.transportCards;
+
+public class TravelPass extends AbstractTravelCard{
+
+    public TravelPass(String ownerName) {
+        super(ownerName);
+    }
+
+    public void writeOff() {
+
+    }
+}
