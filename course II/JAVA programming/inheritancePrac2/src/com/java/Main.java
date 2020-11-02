d676fff9cc15bba577f12f8948ee196a53db0022
Alexey Kovalev <41exey.kov41ev@gmail.com>
2020-11-03 00:34:13 +0300

    inheritancePrac2

---
+++ course II/JAVA programming/inheritancePrac2/src/com/java/Main.java
@@ -0,0 +1,8 @@
+package com.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO: В процессе...
+    }
+}
---
+++ course II/JAVA programming/inheritancePrac2/src/com/java/transportCards/TransportCard.java
@@ -0,0 +1,12 @@
+package com.java.transportCards;
+
+public class TransportCard extends AbstractTravelCard{
+
+    public TransportCard(String ownerName) {
+        super(ownerName);
+    }
+
+    public void writeOff() {
+
+    }
+}
