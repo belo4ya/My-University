6ec5567e15dc9c17d8ddb9ee6b0a4172593cc3cc
Alexey Kovalev <41exey.kov41ev@gmail.com>
2020-11-26 10:58:45 +0300

    add dbms

--- course II/BASH linux/date.sh
+++ course II/BASH linux/date.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # cron:
-# * * * * * /home/belo4ya/bin/date.sh >/dev/null 2>&1  # создание файла с текущим временем в директории ~/tmp/
+# * * * * * /home/belo4ya/bin/date.sh  # создание файла с текущим временем в директории ~/tmp/
 
 
 DATE="$(date  +%H-%M-%S)"
 echo "$DATE" > "$HOME/tmp/$DATE.txt"
-find "$HOME/tmp/" -type f -mmin +17 -exec rm {} \;
+find "$HOME/tmp/" -maxdepth 1 -type f -mmin +17 -exec rm {} \;
