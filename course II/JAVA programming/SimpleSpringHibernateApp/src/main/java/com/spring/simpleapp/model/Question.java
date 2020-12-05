1990dd68a86dc4b25b5af00c3ee678b6bf61d192
Alexey Kovalev <41exey.kov41ev@gmail.com>
2020-12-05 13:16:03 +0300

    spring

---
+++ course II/JAVA programming/SimpleSpringHibernateApp/src/main/java/com/spring/simpleapp/model/Question.java
@@ -0,0 +1,55 @@
+package com.spring.simpleapp.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="questions")
+public class Question extends AuditModel {
+    @Id
+    @GeneratedValue(generator = "question_generator")
+    @SequenceGenerator(
+            name = "question_generator",
+            sequenceName = "Question_sequence",
+            initialValue = 1000
+    )
+    private Long id;
+
+    @Column(columnDefinition = "text")
+    private String title;
+
+    @Column(columnDefinition = "text")
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
