180eb944dbd6d6cea2c0e48f178b7edce71dbdf1
Alexey Kovalev <41exey.kov41ev@gmail.com>
2021-03-31 22:52:44 +0300

    подсмотрел аннотации @CreationTimestamp и @UpdateTimestamp
    кол-во кода сократилось до 150 строк

--- II курс/[Java] Современные технологии программирования/best-rest/src/main/java/com/rest/bestrest/models/SuperEntity.java
+++ II курс/[Java] Современные технологии программирования/best-rest/src/main/java/com/rest/bestrest/models/SuperEntity.java
@@ -3,6 +3,8 @@ package com.rest.bestrest.models;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -19,6 +21,10 @@ public class SuperEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @CreationTimestamp
     private Calendar creationTime;
+
+    @UpdateTimestamp
     private Calendar updateTime;
 }
