395372edb812d2cc30972428977ae60cbc0d68f6
Alexey Kovalev <41exey.kov41ev@gmail.com>
2021-05-22 10:56:53 +0300

    control_work python & sql

---
+++ II курс/[python, SQL] Программирование на языках Python и SQL/control_work/src/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+from sqlalchemy.orm.session import sessionmaker, Session
+from models import Base
+
+
+class DataBase:
+    DATABASE = "sqlite"
+    DB_NAME = "../resources/students.db"
+    engine: Engine = create_engine(f"{DATABASE}:///{DB_NAME}", echo=False)
+    session: Session = sessionmaker(bind=engine)()
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(DataBase.engine)
