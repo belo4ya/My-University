dcf1463b21f6bb6b740978ad09dd7d8578d081ce
belo4ya <41exey.kov41ev@gmail.com>
2021-09-18 02:33:20 +0300

    Эконометрика ff

--- III Курс/(python) Эконометрика/01_reduced_matrix_form/src/task_2_2.py
+++ III Курс/(python) Эконометрика/01_reduced_matrix_form/src/task_2_2.py
@@ -1,44 +1,45 @@
 """
-Гипотетическая модель экономики:
-C = a_1 + b_11 * Y + b_12 * J
-J = a_2 + b_21 * Y_t-1
-T = a_3 + b_31 * Y
-Y = C + J + G
+Модель денежного и товарного рынков:
+R = a_1 + b_12 * Y + b_14 * M               (1)
+Y = a_2 + b_21 * R + b_23 * I + b_25 * G    (2)
+I = a_3 + b_31 * R                          (3)
 
-C - совокупное потребление в период t
-Y - совокупный доход в период t
-J - инвестиции в период t
-T - налог в период t
-G - государственные доходы в период t
+R - процентные ставки
+Y - реальный ВВП
+M - денежная масса
+I - внутренние инвестиции
+G - реальные государственные расходы
+
+(1) - f денежного рынка
+(2) - f товарного рынка
+(3) - f инвестиций
 """
 from pathlib import Path
 
 from sympy import Matrix, solve, Eq
 
-import utils
+from . import utils
 
 # =========================== ДАНО ===========================
 
-endos = 'C J T Y'
+endos = 'R Y I'
 endos = utils.get_symbols_dict(endos)
 
-predeters = 'Y_t-1 G'
+predeters = 'M G'
 predeters = utils.get_symbols_dict(predeters)
 
-params = 'a_1 a_2 a_3 b_11 b_12 b_21 b_31'
+params = 'a_1 a_2 a_3 b_12 b_14 b_21 b_23 b_25 b_31'
 params = utils.get_symbols_dict(params)
 
 # =========================== СТРУКТУРНАЯ ФОРМА (система) ===========================
 
 eq_system = [
-    Eq(endos['C'],
-       params['a_1'] + params['b_11'] * endos['Y'] + params['b_12'] * endos['J']),
-    Eq(endos['J'],
-       params['a_2'] + params['b_21'] * predeters['Y_t-1']),
-    Eq(endos['T'],
-       params['a_3'] + params['b_31'] * endos['Y']),
+    Eq(endos['R'],
+       params['a_1'] + params['b_12'] * endos['Y'] + params['b_14'] * predeters['M']),
     Eq(endos['Y'],
-       endos['C'] + endos['J'] + predeters['G']),
+       params['a_2'] + params['b_21'] * endos['R'] + params['b_23'] * endos['I'] + params['b_25'] * predeters['G']),
+    Eq(endos['I'],
+       params['a_3'] + params['b_31'] * endos['R']),
 ]
 
 # =========================== ПРИВЕДЕННАЯ ФОРМА (система) ===========================
@@ -52,25 +53,22 @@ X = utils.create_predeters_v([1], predeters)
 
 # матрица коэффициентов при ЭНДОГЕННЫХ переменных
 A = Matrix([
-    [1, -params['b_12'], 0, -params['b_11']],
-    [0, 1, 0, 0],
-    [0, 0, 1, -params['b_31']],
-    [-1, -1, 0, 1],
+    [1, -params['b_12'], 0],
+    [-params['b_21'], 1, -params['b_23']],
+    [-params['b_31'], 0, 1]
 ])
 
 # матрица коэффициентов при ПРЕДОПРЕДЕЛЕННЫХ (и мнимых) переменных
 B = Matrix([
-    [-params['a_1'], 0, 0],
-    [-params['a_2'], -params['b_21'], 0],
+    [-params['a_1'], -params['b_14'], 0],
+    [-params['a_2'], 0, -params['b_25']],
     [-params['a_3'], 0, 0],
-    [0, 0, -1],
 ])
 
 U = Matrix([
     [0],
     [0],
-    [0],
-    [0],
+    [0]
 ])
 
 # =========================== ПРИВЕДЕННАЯ ФОРМА (матрица) ===========================
@@ -84,9 +82,10 @@ assert reduced_form_system == solve(reduced_form_matrix, *endos.values())
 
 # =========================== СОХРАНЕНИЕ PNG ===========================
 
-OUTPUT_FOLDER = f'../resources/{Path(__file__).stem}'
+if __name__ == '__main__':
+    OUTPUT_FOLDER = f'../resources/{"_".join(Path(__file__).stem.split("_")[-2:])}'
 
-utils.save_reduced_system(reduced_form_system, OUTPUT_FOLDER)
+    utils.save_reduced_system(reduced_form_system, OUTPUT_FOLDER)
 
-utils.save_components(OUTPUT_FOLDER, A=A, Y=Y, B=B, X=X, U=U)
-utils.save_reduced_matrix(reduced_form_matrix, OUTPUT_FOLDER, dvioptions=['-T', '20.5cm,4.5cm', '-O', '3cm,4cm'])
+    utils.save_components(OUTPUT_FOLDER, A=A, Y=Y, B=B, X=X, U=U)
+    utils.save_reduced_matrix(reduced_form_matrix, OUTPUT_FOLDER, dvioptions=['-T', '20.5cm,4.5cm', '-O', '3cm,4cm'])
