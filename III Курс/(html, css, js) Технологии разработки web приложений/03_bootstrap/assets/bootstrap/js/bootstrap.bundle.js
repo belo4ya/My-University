d641fc6c5c50443dd227bab6ef31d6b32e291013
belo4ya <41exey.kov41ev@gmail.com>
2021-10-01 00:13:35 +0300

    bootstrap

--- III Курс/(html, css, js) Технологии разработки web приложений/03_bootstrap/assets/bootstrap/js/bootstrap.bundle.js
+++ III Курс/(html, css, js) Технологии разработки web приложений/03_bootstrap/assets/bootstrap/js/bootstrap.bundle.js
@@ -11,7 +11,7 @@
 
   /**
    * --------------------------------------------------------------------------
-   * Bootstrap (v5.1.1): util/index.js
+   * Bootstrap (v5.1.1): util/script.js
    * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
    * --------------------------------------------------------------------------
    */
