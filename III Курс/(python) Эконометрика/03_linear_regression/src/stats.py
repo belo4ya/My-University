4e12f44546ab5651a54623d32bf7f30e9fbe8ee0
belo4ya <41exey.kov41ev@gmail.com>
2021-10-03 09:08:01 +0300

    эконометрика + latex

--- III Курс/(python) Эконометрика/03_linear_regression/src/stats.py
+++ III Курс/(python) Эконометрика/03_linear_regression/src/stats.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
-import scipy
-import statsmodels.api as sm
+import warnings
+from functools import cached_property
+
 import numpy as np
 import pandas as pd
-import warnings
+import scipy
+import statsmodels.api as sm
 
 
 def add_constant(x: pd.Series | pd.DataFrame) -> pd.DataFrame:
@@ -14,4 +16,191 @@ def add_constant(x: pd.Series | pd.DataFrame) -> pd.DataFrame:
     return x
 
 
-sm.regression.linear_model.RegressionResults
+class LinearRegression:
+
+    def __init__(self, y: pd.Series, x: pd.Series | pd.DataFrame):
+        self._y = y
+        self._x = x
+        self._model = sm.OLS(self._y, self._x)
+        self._results = None
+
+    def fit(self):
+        self._results = self._model.fit()
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+    @property
+    def results(self):
+        return self._results
+
+    def summary(self):
+        return self._results.summary()
+
+    @property
+    def params(self):
+        r"""
+        $ \hat{b_i} $ - оценки параметров модели
+
+        """
+        return self._results.params
+
+    @property
+    def bse(self):
+        r"""
+        $ \hat{S_{b_i}} $ - оценки среднеквадратического отклонения оценок параметров$
+
+        """
+        return self._results.bse
+
+    @cached_property
+    def rmsd_resid(self):
+        """
+        $ S_{e_i} $ - оценка среднеквадратического отклонения возмущений
+
+        """
+        return np.sqrt(self._results.mse_resid)
+
+    @property
+    def rsquared(self):
+        """
+        $ R^2 $ - коэффициент детерминации
+
+        """
+        return self._results.rsquared
+
+    @cached_property
+    def mapprxe(self):
+        """
+        $ A $ - средняя относительная ошибка аппроксимации
+
+        """
+        return np.mean(np.abs(self._results.resid / self._y)) * 100
+
+    @property
+    def fvalue(self):
+        """
+        $ F_{набл} $ - F-критерий Фишера
+
+        """
+        return self._results.fvalue
+
+    def ftable(self, alpha=0.05):
+        """
+        $ F_{табл} $ - табличное значение F-критерия Фишера
+
+        :param alpha: уровень значимости
+        """
+        return scipy.stats.f.ppf(1 - alpha, self._results.nobs, self._results.df_resid)  # noqa
+
+    @property
+    def f_pvalue(self):
+        """
+        $ p{-value}_F $ - p-value для F-критерия Фишера
+
+        """
+        return self._results.f_pvalue
+
+    @property
+    def tvalues(self):
+        """
+        $ t_{iнабл} $ - значения статистики t-критерия Стьюдента
+
+        """
+        return self._results.tvalues
+
+    def ttable(self, alpha=0.05):
+        """
+        $ t_{табл} $ - табличное значение статистики t-критерия Стьюдента
+
+        :param alpha: уровень значимости
+        """
+        return scipy.stats.t.ppf(1 - alpha / 2, self._results.df_resid)  # noqa
+
+    @property
+    def t_pvalues(self):
+        """
+        $ p{-value}_{ti} $ - p-value для t-критерия Стьюдента для оценки значимости параметров модели
+
+        """
+        return self._results.pvalues
+
+    def conf_int(self, alpha=0.05, cols=None):
+        """
+        Доверительные интервалы
+
+        :param alpha: уровень доверительного интервала. По умолчанию 95%
+        :param cols:
+        """
+        return self._results.conf_int(alpha=alpha, cols=cols)
+
+    @cached_property
+    def beta(self):
+        """
+        $ \tilde{b_i} $
+
+
+        """
+        return (self.params * self.bse / self._y.std()).drop('const', errors='ignore')
+
+    @cached_property
+    def delta(self):
+        r"""
+        $ \Delta_i $
+
+
+        """
+        return (self._x.corrwith(self._y) * self.params / self.rsquared).drop('const', errors='ignore')
+
+    @cached_property
+    def elasticity(self):
+        """
+        $ Э_i $
+
+
+        """
+        return (self.params * self._x.mean() / self._y.mean()).drop('const', errors='ignore')
+
+
+if __name__ == '__main__':
+    y = pd.Series(
+        [15, 18, 11, 300, 15, 18, 11, 300, 15, 18, 11, 300, 15, 18, 11, 300, 15, 18, 11, 300, 15, 18, 11, 300]
+    )
+
+    x = pd.DataFrame([[2, 3],
+                      [2, 3],
+                      [1, 4],
+                      [6, 7],
+                      [2, 3],
+                      [2, 3],
+                      [1, 4],
+                      [6, 7],
+                      [2, 3],
+                      [2, 3],
+                      [1, 4],
+                      [6, 7],
+                      [2, 3],
+                      [2, 3],
+                      [1, 4],
+                      [6, 7],
+                      [2, 3],
+                      [2, 3],
+                      [1, 4],
+                      [6, 7],
+                      [2, 3],
+                      [2, 3],
+                      [1, 4],
+                      [6, 7]])
+
+    model = LinearRegression(y, add_constant(x))
+    print(model.summary())
+    print(model.conf_int())
+    print(model.beta)
+    print(model.delta)
+    print(model.elasticity)
+    print(model.params)
