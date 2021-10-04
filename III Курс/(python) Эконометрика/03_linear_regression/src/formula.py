66ff6783d61ba9598dbbf2f6131c4c7c5b4a6d8a
belo4ya <41exey.kov41ev@gmail.com>
2021-10-04 03:21:54 +0300

    f-test в истории

--- III Курс/(python) Эконометрика/03_linear_regression/src/formula.py
+++ III Курс/(python) Эконометрика/03_linear_regression/src/formula.py
@@ -1,9 +1,6 @@
-
 # МНК
 OLS = r'\sum_{i}^{n}{(y_i - \hat{y_i})^2} \rightarrow \min_{n}'
 OLS_M = 'B = (X^T X)^{-1} X^T Y'
 
 # F-критерий Фишера
 F_VALUE = r'F = \frac{\frac{R^2}{k}}{\frac{1 - R^2}{(n - k - 1)}}'
-
-
