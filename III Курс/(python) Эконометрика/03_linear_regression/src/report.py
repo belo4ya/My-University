cf4a737ef98d1678036f2d742bc1bf3ca551caf6
belo4ya <41exey.kov41ev@gmail.com>
2021-10-08 02:51:12 +0300

    эконометрика auto

--- III Курс/(python) Эконометрика/03_linear_regression/src/report.py
+++ III Курс/(python) Эконометрика/03_linear_regression/src/report.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from IPython.display import Markdown
 
 from src._components import to_math, CONF_INT, PrettyModel, FTest, TTest
@@ -23,6 +24,32 @@ def general_assessment(model: LinearRegression, precision: int = 3) -> Markdown:
         pretty_model.rsquared(inline=True),
         pretty_model.mapprxe(inline=True)
     ]))
+
+
+def rmsd_resid(model: LinearRegression, precision: int = 3) -> Markdown:
+    assessment = {
+        pd.Interval(0, 2): '10/10',
+        pd.Interval(2, 5): '8/10',
+        pd.Interval(5, 10): '7.5/10',
+        pd.Interval(10, 15): '3.5/10',
+        pd.Interval(15, 30): '0.8/10',
+        None: '0/10'
+    }
+    percent = model.rmsd_resid / model.y.mean() * 100
+    for k, v in assessment.items():
+        if k is None:
+            print(percent, k)
+        elif percent in k:
+            print(percent, k)
+            break
+
+
+def rsquared(model: LinearRegression, precision: int = 3) -> Markdown:
+    pass
+
+
+def mapprxe(model: LinearRegression, precision: int = 3) -> Markdown:
+    pass
 
 
 def f_test(model: LinearRegression, precision: int = 3, alpha: float = 0.05) -> Markdown:
