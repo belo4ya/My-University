220c1a100163ff7d4c39e80b89521493aab191e9
belo4ya <41exey.kov41ev@gmail.com>
2021-10-09 11:18:20 +0300

    data 6 db

--- III Курс/(python) Технологии обработки больших данных/06_database/main.py
+++ III Курс/(python) Технологии обработки больших данных/06_database/main.py
@@ -1,10 +1,7 @@
-import sqlite3
-
-con = sqlite3.connect('data/Chinook_Sqlite.sqlite')
-cur = con.cursor()
-
-cur.execute('SELECT name FROM sqlite_master WHERE type="table"')
-print(cur.fetchall())
-
-con.commit()
-con.close()
+a = '18.0'
+print(a)
+print(a.isnumeric())
+print(a.isdecimal())
+print(a.isdigit())
+print(float(a))
+print(int(a))
