a4530d6b13ec5227983bc0b40dcb6f0a99c538e4
belo4ya <41exey.kov41ev@gmail.com>
2021-10-09 03:59:11 +0300

    эконометрика auto

--- III Курс/(python) Эконометрика/03_linear_regression/src/_components/__init__.py
+++ III Курс/(python) Эконометрика/03_linear_regression/src/_components/__init__.py
@@ -1,4 +1,4 @@
-from .formula import OLS, OLS_M, F_VALUE, T_VALUE, CONF_INT
+from .formula import OLS, OLS_M, F_VALUE, T_VALUE, CONF_INT, RMSD_RESID, RSQUARED, MAPPRXE
 from .pretty import PrettyModel
 from .test_ import FTest, TTest
 from .utils import to_math
