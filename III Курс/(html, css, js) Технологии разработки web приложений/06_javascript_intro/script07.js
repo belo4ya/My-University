9d7df9761597c8cdbe1bc996f4f722d4102868ad
belo4ya <41exey.kov41ev@gmail.com>
2021-12-06 01:53:00 +0300

    js art 🎨

--- III Курс/(html, css, js) Технологии разработки web приложений/06_javascript_intro/script07.js
+++ III Курс/(html, css, js) Технологии разработки web приложений/06_javascript_intro/script07.js
@@ -5,13 +5,13 @@ const reverseArray = (arr) => {
 
 const reverseArrayInPlace = (arr) => {
   const length = arr.length;
-  for (let i = 0; i < length / 2; i++) {
-    [arr[i], arr[length - 1 - i]] = [arr[length - 1 - i], arr[i]];
+  for (let i = 0, j = length; i < length / 2; i++, j--) {
+    [arr[i], arr[j - 1]] = [arr[j - 1], arr[i]];
   }
 }
 
 
-const arr = [1, 2, -3, -5, 7, 10]
+const arr = [1, 2, -3, -5, 7, 10];
 console.log(reverseArray(arr));
 reverseArrayInPlace(arr);
 console.log(arr);
