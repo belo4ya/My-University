62ac3e3eda177cb88791b9bacded1ad3f7cdfb01
belo4ya <41exey.kov41ev@gmail.com>
2021-12-07 23:57:00 +0300

    js art 🎨

---
+++ III Курс/(html, css, js) Технологии разработки web приложений/06_javascript_intro/script13.js
@@ -0,0 +1,18 @@
+const some = (arr, predicate) => {
+  for (const item of arr) {
+    if (predicate(item)) return true;
+  }
+  return false;
+}
+
+const every = (arr, predicate) => {
+  for (const item of arr) {
+    if (!predicate(item)) return false;
+  }
+  return true;
+}
+
+console.log(every([NaN, NaN, NaN], isNaN));
+console.log(every([NaN, NaN, 4], isNaN));
+console.log(some([NaN, 3, 4], isNaN));
+console.log(some([2, 3, 4], isNaN));
