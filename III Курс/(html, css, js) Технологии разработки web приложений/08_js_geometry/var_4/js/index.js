c57f0498960c28adf6c5ae5a5675dc41c6ad3f98
belo4ya <41exey.kov41ev@gmail.com>
2021-12-16 13:48:00 +0300

    js variant-13

--- III Курс/(html, css, js) Технологии разработки web приложений/08_js_geometry/var_4/js/index.js
+++ III Курс/(html, css, js) Технологии разработки web приложений/08_js_geometry/var_4/js/index.js
@@ -1,6 +1,5 @@
 const API_URL = `https://apidata.mos.ru/v1/datasets/2985/features?api_key=484d27f7fe880f088a369b7d5cb0af55`;
 
-
 const clear = (canvas) => {
     const ctx = canvas.getContext('2d');
     ctx.save();
@@ -15,6 +14,18 @@ const drawPoint = (canvas, {x, y, r, color}) => {
     ctx.beginPath();
     ctx.arc(x, y, r, 0, Math.PI * 2, true);
     ctx.fill();
+}
+
+const drawHorizontalLine = (canvas, {begin, end, width, color}) => {
+    const ctx = canvas.getContext('2d');
+
+    ctx.strokeStyle = color;
+    ctx.lineWidth = width;
+
+    ctx.beginPath();
+    ctx.moveTo(begin.x, begin.y);
+    ctx.lineTo(end.x, end.y);
+    ctx.stroke();
 }
 
 const fetchData = async (url) => {
@@ -92,24 +103,46 @@ const generatePoints = (n) => {
     });
 }
 
-const distance = (pointA, pointB) => {
-    return Math.sqrt((pointA.x - pointB.x) ** 2 + (pointA.y - pointB.y) ** 2)
+const getHorizontalLine = (points, color = 'rgb(253, 224, 71, 0.5)') => {
+    const sorted = points.sort(({y: aY}, {y: bY}) => aY - bY);
+    const i = Math.floor(sorted.length / 2);
+    let y;
+    if (sorted.length % 2) {
+        y = Math.round((sorted[i + 1].y - sorted[i - 1].y) / 2 + sorted[i - 1].y);
+        console.log(`\nВыше: ${sorted.slice(i + 1).length}, на: 1, ниже: ${sorted.slice(0, i).length}`);
+    } else {
+        y = Math.round((sorted[i].y - sorted[i - 1].y) / 2 + sorted[i - 1].y);
+        console.log(`\nВыше: ${sorted.slice(i).length}, на: 0, ниже: ${sorted.slice(0, i).length}`);
+    }
+    const canvas = document.getElementById('js-canvas');
+    const lineBegin = {x: 0, y: y};
+    const lineEnd = {x: canvas.width, y: y};
+    return {begin: lineBegin, end: lineEnd, width: 2, color: color};
 }
 
-const getMaxCircle = (points, color = 'rgb(192, 38, 211, 0.3)') => {
-    const pointsMap = new Map();
-    for (let i = 0; i < points.length; i++) {
-        let minDistance = 100_000_000;
-        for (let j = 0; j < points.length; j++) {
-            if (i !== j) {
-                minDistance = Math.min(minDistance, distance(points[i], points[j]));
-            }
-        }
-        pointsMap.set(points[i], minDistance);
+const showStat = (data) => {
+    const adminAreaCounter = new Map();
+    const districtCounter = new Map();
+    for (const station of data) {
+        adminAreaCounter.set(
+            station.admArea,
+            adminAreaCounter.get(station.admArea) === undefined ? 0 : adminAreaCounter.get(station.admArea) + 1
+        );
+        districtCounter.set(
+            station.district,
+            districtCounter.get(station.district) === undefined ? 0 : districtCounter.get(station.district) + 1
+        );
     }
 
-    const sorted = [...pointsMap.entries()].sort(([, v1], [, v2]) => v2 - v1);
-    return {...sorted[0][0], r: sorted[0][1], color: color};
+    console.log('Статистика по округам:');
+    for (const [k, v] of adminAreaCounter.entries()) {
+        console.log(`${k}: ${v} (${(v / data.length * 100).toFixed(2)}%)`);
+    }
+
+    console.log('\nСтатистика по районам:');
+    for (const [k, v] of districtCounter.entries()) {
+        console.log(`${k}: ${v} (${(v / data.length * 100).toFixed(2)}%)`);
+    }
 }
 
 const drawCoordinates = (data) => {
@@ -117,6 +150,8 @@ const drawCoordinates = (data) => {
 
     const dataChecked = document.getElementById('js-checkbox-data').checked;
     if (dataChecked) {
+        console.clear();
+        showStat(data);
         points.push(...makePointsFromData(data));
     }
 
@@ -132,7 +167,7 @@ const drawCoordinates = (data) => {
         for (const point of points) {
             drawPoint(canvas, point);
         }
-        drawPoint(canvas, getMaxCircle(points));
+        drawHorizontalLine(canvas, getHorizontalLine(points));
     }
 }
 
