326109e3c257bfd54ac943f05bb9c54d5dc18b03
belo4ya <41exey.kov41ev@gmail.com>
2022-02-14 10:18:45 +0300

    🔥

---
+++ III Курс/(java) Мобильная разработка/00_app_on_fire/AppOnFire/app/src/main/java/com/belo4ya/apponfire/MainActivity.java
@@ -0,0 +1,14 @@
+package com.belo4ya.apponfire;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
