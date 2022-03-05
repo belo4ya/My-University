a2214ffcff4a7eed66be348a4cd169a94533bd6d
belo4ya <41exey.kov41ev@gmail.com>
2022-03-05 06:28:39 +0300

    что-то

---
+++ III Курс/(java) Мобильная разработка/04_recycler_view/RecyclerView/app/src/main/java/com/belo4ya/recyclerview/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.belo4ya.recyclerview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        recyclerView = findViewById(R.id.list)
+    }
+}
