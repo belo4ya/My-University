25f98e8f73d37856d9464823336ee0c0d05a706d
belo4ya <41exey.kov41ev@gmail.com>
2022-03-20 15:29:56 +0300

    что-то

--- III Курс/(java) Мобильная разработка/04_recycler_view/RecyclerView/app/src/main/java/com/belo4ya/recyclerview/MainActivity.kt
+++ III Курс/(java) Мобильная разработка/04_recycler_view/RecyclerView/app/src/main/java/com/belo4ya/recyclerview/MainActivity.kt
@@ -2,19 +2,33 @@ package com.belo4ya.recyclerview
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var recyclerView: RecyclerView
+    private val profiles = generateProfiles().toMutableList()
+    private val profileAdapter = ProfileAdapter(profiles)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initViews()
+        val profilesRecyclerView = findViewById<RecyclerView>(R.id.profiles_list)
+        profilesRecyclerView.adapter = profileAdapter
+        profilesRecyclerView.layoutManager = LinearLayoutManager(this)
     }
+}
 
-    private fun initViews() {
-        recyclerView = findViewById(R.id.list)
-    }
+private fun generateProfiles(): List<Profile> {
+    return listOf(
+        Profile("Алексей", "Ковалев", R.drawable.avatar_placeholder, ""),
+        Profile("Алексей", "Ковалев", R.drawable.avatar_placeholder, ""),
+        Profile("Алексей", "Ковалев", R.drawable.avatar_placeholder, ""),
+        Profile("Алексей", "Ковалев", R.drawable.avatar_placeholder, ""),
+        Profile("Алексей", "Ковалев", R.drawable.avatar_placeholder, ""),
+        Profile("Алексей", "Ковалев", R.drawable.avatar_placeholder, ""),
+        Profile("Алексей", "Ковалев", R.drawable.avatar_placeholder, ""),
+        Profile("Алексей", "Ковалев", R.drawable.avatar_placeholder, ""),
+        Profile("Алексей", "Ковалев", R.drawable.avatar_placeholder, ""),
+    )
 }
---
+++ III Курс/(java) Мобильная разработка/04_recycler_view/RecyclerView/app/src/main/java/com/belo4ya/recyclerview/Profile.kt
@@ -0,0 +1,3 @@
+package com.belo4ya.recyclerview
+
+data class Profile(val firstName: String, val lastName: String, val avatar: Int, val sex: String)
