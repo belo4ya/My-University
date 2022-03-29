ca535e33b02274f96692749c7caf6ae7c1d33e95
belo4ya <41exey.kov41ev@gmail.com>
2022-03-29 13:14:56 +0300

    react todo-app

---
+++ III Курс/(html, css, js) Технологии разработки web приложений/10_todo_app/react-todo-app-master/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/wrappers/App';
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/style/index.css';
+
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
