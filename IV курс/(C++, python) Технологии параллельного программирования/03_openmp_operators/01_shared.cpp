6cc3cd745197fe50e07ac1c78520fb811eec7dd9
belo4ya <41exey.kov41ev@gmail.com>
2022-10-04 19:21:00 +0300

    c++ 3

---
+++ IV курс/(C++, python) Технологии параллельного программирования/03_openmp_operators/01_shared.cpp
@@ -0,0 +1,13 @@
+#include <omp.h>
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string hw = "Hello World!\n";
+#pragma omp parallel shared(hw)
+  { cout << hw; }
+  return 0;
+}
