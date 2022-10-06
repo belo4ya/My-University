7dc4e29ad0e37c6972ae1b6e135b151488d773c4
belo4ya <41exey.kov41ev@gmail.com>
2022-10-06 20:00:00 +0300

    c++ 5

---
+++ IV курс/(C++, python) Технологии параллельного программирования/05_openmp_sync/02_master.cpp
@@ -0,0 +1,32 @@
+#include <omp.h>
+#include <stdio.h>
+
+#include <locale>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    int n;
+
+#pragma omp parallel private(n)
+
+    {
+        n = 1;
+
+#pragma omp master
+
+        { n = 2; }
+
+        printf("Первое значение n потока %d: %d\n", omp_get_thread_num(), n);
+
+#pragma omp barrier
+
+#pragma omp master
+
+        { n = 3; }
+
+        printf("Второе значение n потока %d: %d\n", omp_get_thread_num(), n);
+    }
+
+    return 0;
+}
