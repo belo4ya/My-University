1ff6d3e89f53e5a1c833b37bed1f4b34303f8940
belo4ya <41exey.kov41ev@gmail.com>
2022-10-14 23:00:00 +0300

    c++ 6

---
+++ IV курс/(C++, python) Технологии параллельного программирования/06_openmp_schedule/01_static.cpp
@@ -0,0 +1,30 @@
+#include <omp.h>
+#include <stdio.h>
+#include <windows.h>
+
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+int main()
+
+{
+    setlocale(LC_ALL, "Russian");
+
+    int i;
+
+    double time = omp_get_wtime();
+
+#pragma omp parallel private(i) num_threads(4)
+
+    {
+#pragma omp for schedule(static, 6)
+        for (i = 0; i < 200; i++) {
+            printf("Поток %d выполнила итерацию %d\n", omp_get_thread_num(), i);
+            Sleep(100 * (omp_get_thread_num() + 1));
+        }
+    }
+
+    cout << "Time = " << (omp_get_wtime() - time) << endl;
+}
