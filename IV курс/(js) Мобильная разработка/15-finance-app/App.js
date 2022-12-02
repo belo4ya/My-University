ad6b20a92b3608ec802b47b08bad4b202dcc4fc5
belo4ya <41exey.kov41ev@gmail.com>
2022-12-02 19:34:00 +0300

    mobile-15: progress

--- IV курс/(js) Мобильная разработка/15-finance-app/App.js
+++ IV курс/(js) Мобильная разработка/15-finance-app/App.js
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet} from 'react-native';
+import {NavigationContainer} from "@react-navigation/native";
+import { Button, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import React from 'react';
+
+const Drawer = createDrawerNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen}/>
+        <Drawer.Screen name="Notifications" component={NotificationsScreen}/>
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
